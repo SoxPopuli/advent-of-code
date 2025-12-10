c100ed2533f7c622dc2309cde6376157204c49a4
Charlotte Smith <charliesmith5019@gmail.com>
2025-12-10 04:00:57 +0000

    2025-09 pt2

--- 2025/09/src/main.rs
+++ 2025/09/src/main.rs
@@ -1,4 +1,5 @@
 use common::Pos;
+use std::collections::HashMap;
 
 fn parse_input(input: &str) -> Vec<Pos> {
     input
@@ -26,6 +27,38 @@ impl Rect {
 
         x_diff * y_diff
     }
+
+    fn top_left(&self) -> Pos {
+        Pos {
+            x: self.a.x.min(self.b.x),
+            y: self.a.y.min(self.b.y),
+        }
+    }
+
+    fn bottom_right(&self) -> Pos {
+        Pos {
+            x: self.a.x.max(self.b.x),
+            y: self.a.y.max(self.b.y),
+        }
+    }
+
+    fn contains(&self, point: Pos) -> bool {
+        let top_left = self.top_left();
+        let bottom_right = self.bottom_right();
+
+        point.x > top_left.x
+            && point.x < bottom_right.x
+            && point.y > top_left.y
+            && point.y < bottom_right.y
+    }
+
+    fn contains_midpoint(&self, start: Pos, end: Pos) -> bool {
+        let midpoint = Pos {
+            x: (start.x + end.x) / 2,
+            y: (start.y + end.y) / 2,
+        };
+        self.contains(midpoint)
+    }
 }
 
 fn part1(tiles: &[Pos]) -> usize {
@@ -43,17 +76,70 @@ fn part1(tiles: &[Pos]) -> usize {
     rects.into_iter().map(|x| x.size()).max().unwrap()
 }
 
+fn part2(points: &[Pos]) -> usize {
+    let mut areas = HashMap::new();
+
+    for i in 0..points.len() {
+        for j in i + 1..points.len() {
+            let rect = Rect {
+                a: points[i],
+                b: points[j],
+            };
+
+            areas.insert((i, j), rect.size());
+        }
+    }
+
+    let sorted = {
+        let mut sorted = areas.into_iter().collect::<Vec<_>>();
+        sorted.sort_by_key(|(_, area)| *area);
+        sorted
+    };
+
+    sorted
+        .into_iter()
+        .rev()
+        .find_map(|((x, y), area)| {
+            let rect = Rect {
+                a: points[x],
+                b: points[y],
+            };
+
+            let contains_point = || points.iter().any(|p| rect.contains(*p));
+
+            let contains_midpoint = || {
+                (0..points.len() - 1)
+                    .map(|i| (points[i], points[i + 1]))
+                    .any(|(a, b)| rect.contains_midpoint(a, b))
+            };
+
+            if !contains_point()
+                && !contains_midpoint()
+                && (!rect.contains_midpoint(points[0], points[points.len() - 1]))
+            {
+                Some(area)
+            } else {
+                None
+            }
+        })
+        .unwrap()
+}
+
 fn main() {
     let input = common::read_stdin();
     let tiles = parse_input(&input);
 
     let (time, result) = common::timed(|| part1(&tiles));
     println!("Part 1: {result} in {time:?}");
+
+    let (time, result) = common::timed(|| part2(&tiles));
+    println!("Part 2: {result} in {time:?}");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn example() {
         let input = r#"
@@ -69,5 +155,7 @@ mod tests {
 
         let tiles = parse_input(input);
         assert_eq!(part1(&tiles), 50);
+
+        assert_eq!(part2(&tiles), 24);
     }
 }
